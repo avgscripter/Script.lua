f5926086f2f519106204c44d1143a138c5ce42fc
avgscripter <applevsapple8790@gmail.com>
2025-03-14 19:08:07 +0530

    Update Jailbreak2.lua

--- Jailbreak2.lua
+++ Jailbreak2.lua
@@ -1,3 +1,3 @@
 
-loadstring(game:HttpGet("https://alsploit.bedwars.workers.dev/MainScript.lua?Key=Gvixgc5T4Bm3PG2c92sjFe0QzHCeln3u8P1I5wisU6FPO6e1az"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/avgscripter/Script.lua/refs/heads/main/Gui.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/avgscripter/Script.lua/refs/heads/main/jailbreak.lua"))()
