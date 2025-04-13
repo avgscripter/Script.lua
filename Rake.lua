75831ef11546b4d367d90dfd57392890b9c3c6cc
avgscripter <applevsapple8790@gmail.com>
2025-04-13 19:27:02 +0530

    Update Rake.lua

--- Rake.lua
+++ Rake.lua
@@ -1,2 +1,3 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/avgscripter/Script.lua/bb9d4241bae2ed65d5dc1c6447bb234c69feb711/Gui.lua",true))()
+wait()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/avgscripter/Script.lua/main/Rake3.lua",true))()
