8e243d746545ab3236994f27a599f3d9f0a93388
avgscripter <applevsapple8790@gmail.com>
2025-04-13 21:48:17 +0530

    Update Rake3.lua

--- Rake3.lua
+++ Rake3.lua
@@ -857,8 +857,8 @@ local instaKillBtn = _G.Main.createButton(Combat, "Insta Kill Rake", function()
         end
     end
 end)
-	end
-	end
+end)
+end
 TextButton2.MouseButton1Click:Connect(function()
 	sapien.Enabled = not sapien.Enabled
 end)
